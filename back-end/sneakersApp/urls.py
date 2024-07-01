7dace1d0a81af600ddcbfdb6831de2d4b76eb1c7
JaxV9 <layanj.pro@gmail.com>
2024-07-01 22:38:11 +0200

    feat: django rest added

---
+++ back-end/sneakersApp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework import routers
+from .views import *
+
+router = routers.DefaultRouter()
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+urlpatterns += router.urls
