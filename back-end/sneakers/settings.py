fe1a0944368cc60797ba5af94d27c58971777a99
JaxV9 <layanj.pro@gmail.com>
2024-07-01 21:40:30 +0200

    docker created

--- back-end/sneakers/settings.py
+++ back-end/sneakers/settings.py
@@ -73,14 +73,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sneakers.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sneakersDb',
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT')
     }
 }
 
