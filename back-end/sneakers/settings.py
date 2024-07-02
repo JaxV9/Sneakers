2737683f2ae139698cf37e11e06b365e8ac4764f
JaxV9 <layanj.pro@gmail.com>
2024-07-02 15:20:06 +0200

    swagger added

--- back-end/sneakers/settings.py
+++ back-end/sneakers/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'corsheaders',
     'sneakersApp',
     'rest_framework_simplejwt',
--- back-end/sneakers/urls.py
+++ back-end/sneakers/urls.py
@@ -18,9 +18,28 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Your API Title",
+      default_version="v1",
+      description="Your API description",
+      terms_of_service="https://example.com/terms/",
+      contact=openapi.Contact(email="contact@example.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('sneakersApp.urls')) 
+    path('', include('sneakersApp.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
