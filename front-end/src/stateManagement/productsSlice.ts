eb668e36ecde0560bb895f8834f42ae2e7d80679
antoine30000 <antoine.beaurin@eemi.com>
2024-07-02 17:41:45 +0200

    wip: data fetching

---
+++ front-end/src/stateManagement/productsSlice.ts
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export interface ShoeMetadata{
+    id: number,
+    name: string,
+    price: number,
+    pictures_product:PictureMetadata[]
+}
+export interface PictureMetadata{
+    id:number;
+    path:string;
+}
+
+export const productsSlice = createApi({
+    baseQuery:fetchBaseQuery({baseUrl:"http://localhost:8000/api"}),
+    endpoints: (builder) => ({
+        getShoes: builder.query<ShoeMetadata[], void[]>({
+          query: () => `/v1/shoes/`,
+        }),
+        getShoesById: builder.query<ShoeMetadata,{id:number}>({
+            query: (id) => `/v1/shoes/${id}`,
+        }),
+    }),
+})
+
+export const {useGetShoesQuery,useGetShoesByIdQuery} = productsSlice
--- front-end/src/stateManagement/store.ts
+++ front-end/src/stateManagement/store.ts
@@ -1,5 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
+import { productsSlice } from "./productsSlice";
 
-// export const store = configureStore({
-//   //   reducer:
-// });
+
+
+export const store = configureStore({
+    reducer: {
+        productsSlice : productsSlice.reducer
+    }
+})
