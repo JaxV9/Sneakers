1ae841f4b37dd230ce7904d70e8941247ac59d57
antoine30000 <antoine.beaurin@eemi.com>
2024-07-02 15:47:52 +0200

    wip: store and cards

--- front-end/src/features/store/Store.tsx
+++ front-end/src/features/store/Store.tsx
@@ -1,7 +1,10 @@
 import { Card } from "./components/Card"
 
 export const Store = () => {
+    const mockTab = [1,2,3,4,5,6,7,8,9]
     return (
-        <div className="flex-container"><Card /></div>
+        <div className="grid-container">
+            {mockTab.map((index)=> <Card key={index}/>)}
+        </div>
     )
 }
