20e4d90a6c681fb7220a343fb647f3fddb1ae2ee
Antoine 3000 <antoine@beaurin.net>
2024-07-02 21:57:19 +0200

    feat: card calculation

--- front-end/src/features/cart/Cart.tsx
+++ front-end/src/features/cart/Cart.tsx
@@ -1,27 +1,43 @@
-import { useSelector } from "react-redux";
-import { CartList } from "./components/CartList"
-import { CartMetadata } from "../../stateManagement/cardSlice";
+import { useSelector } from 'react-redux';
+import { CartList } from './components/CartList';
+import { CartMetadata } from '../../stateManagement/cardSlice';
 
-export const Cart  = () => {
-   const cart = useSelector((state:CartMetadata)=>state.items);
+const TAX_RATE = 0.2; // Example tax rate, adjust as needed
+
+export const Cart = () => {
+    const cart = useSelector((state: CartMetadata) => state.items);
+
+    const totalPrice = cart.reduce((total, item) => total + item.shoe.price * item.numberInCart, 0);
+    const taxes = totalPrice * TAX_RATE;
+    const totalOrder = totalPrice + taxes;
+
     return (
         <div className="flex-container w-full flex-vertical">
-           <h1>Panier</h1>
-           <CartList items={cart}></CartList>
-           <div className="flex-container">
-            <div className="flex-container flex-vertical">
-                <span>Appliquer code de réduction</span>
-                <div className="flex-container">
-                    <button>Continuer mes achats</button>
+            <h1>Panier</h1>
+            <CartList items={cart} />
+            <div className="flex-container">
+                <div className="flex-container flex-vertical">
+                    <span>Appliquer code de réduction</span>
+                    <div className="flex-container">
+                        <button>Continuer mes achats</button>
+                    </div>
+                </div>
+                <div className="flex-container flex-vertical">
+                    <div className="flex-container space-between">
+                        <span>Total</span>
+                        <span>{totalPrice.toFixed(2)}€</span>
+                    </div>
+                    <div className="flex-container space-between">
+                        <span>Taxes</span>
+                        <span>{taxes.toFixed(2)}€</span>
+                    </div>
+                    <div className="flex-container space-between">
+                        <span>TOTAL DE LA COMMANDE</span>
+                        <span>{totalOrder.toFixed(2)}€</span>
+                    </div>
+                    <button>Finaliser ma commande</button>
                 </div>
             </div>
-            <div className="flex-container flex-vertical">
-                <div className="flex-container space-between"><span>Total</span></div>
-                <div className="flex-container space-between"><span>Taxes</span></div>
-                <div className="flex-container space-between"><span>TOTAL DE LA COMMANDE</span></div>
-                <button className="">Finaliser ma commande</button>
-            </div>
-           </div>
         </div>
-    )
-}
+    );
+};
--- front-end/src/features/cart/components/CartList.tsx
+++ front-end/src/features/cart/components/CartList.tsx
@@ -8,12 +8,22 @@ interface CartListProps {
 export const CartList  = (cart:CartListProps) => {
     return (
         <div className="flex-container flex-vertical">
-            {cart.items.map((item) => (
-                <ItemCard 
-                    quantity={item.numberInCart}
-                    key={item.shoe.id} 
-                    shoe={item.shoe}                 
-                />))}
+            <div className="flex-container">
+                <span>Articles ajoutés</span>
+                <div className="flex-container">
+                    <span>Prix unitaire</span>
+                    <span>Quantité</span>
+                    <span>Total</span>
+                </div>
+            </div>
+            <div className="flex-container flex-vertical">
+                {cart.items.map((item) => (
+                    <ItemCard 
+                        quantity={item.numberInCart}
+                        key={item.shoe.id} 
+                        shoe={item.shoe}                 
+                    />))}
+            </div>
         </div>
     )
 }
