33e763f120cd55559d1f04fcc976e57fe58ebabd
JaxV9 <layanj.pro@gmail.com>
2024-07-03 12:33:30 +0200

    working on project page

--- front-end/src/features/Projet/Projet.tsx
+++ front-end/src/features/Projet/Projet.tsx
@@ -2,10 +2,36 @@
 
 export const Projet = () => {
 
-    return(
+
+    return (
         <>
             <section className="heroProjectContainer">
                 <div className="heroBackgroundImage"></div>
+                <div className="heroTagContainer">
+                    <span>Veja EcoCollect</span>
+                </div>
+            </section>
+            <section className="projectContentContainer">
+                <h2>Notre mission</h2>
+                <span className="hashTagContent">#walkforabetterfutur</span>
+                <div className="introProjectContentContainer">
+                    <p>Ce matériau est utilisé dans la
+                        semelle de chaque basket. Nous
+                        achetons le caoutchouc directement
+                        auprès de coopératives formées par
+                        des familles de seringueiros
+                        (récolteurs de caoutchouc).
+                        <br /><br />
+                        Ce matériau est utilisé dans le
+                        aoutchouc.Ce matériau est utilisé dans la
+                        semelle de chaque basket. Nous
+                        achetons le caoutchouc directement
+                        auprès de coopératives formées par
+                        des familles de seringueiros
+                        (récolteurs de caoutchouc).</p>
+                        <div className="introProjectImage"></div>
+                </div>
+                
             </section>
         </>
     )
